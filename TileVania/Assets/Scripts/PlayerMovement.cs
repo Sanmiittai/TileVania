f928fd02e732c3bf23e8f2944cf203200d2b7ad8
Sanmiittai <santhedev@gmail.com>
2022-10-14 14:33:53 -0300

    Create tilemaps, player, player animation and player movement

---
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+    Rigidbody2D rb;
+    float speed = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * speed, rb.velocity.y);
+        rb.velocity = playerVelocity;
+    }
+}
