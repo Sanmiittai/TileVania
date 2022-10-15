8054d2d80c78da83044b295742c020fc4e615372
Sanmiittai <santhedev@gmail.com>
2022-10-15 12:22:05 -0300

    Create movement, animations and lumberjack

--- TileVania/Assets/Scripts/PlayerMovement.cs
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -8,15 +8,26 @@ public class PlayerMovement : MonoBehaviour
     Vector2 moveInput;
     Rigidbody2D rb;
     float speed = 10f;
+    Animator myAnimator;
+    float jumpSpeed = 19f;
+    CapsuleCollider2D myCapsuleCollider;
+    float climbSpeed = 5f;
+    float myGravity = 5f;
+    float climbingGravity = 0f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
+        myGravity = rb.gravityScale;
     }
 
     void Update()
     {
         Run();
+        FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -24,9 +35,48 @@ public class PlayerMovement : MonoBehaviour
         moveInput = value.Get<Vector2>();
     }
 
+    void OnJump(InputValue value)
+    {
+        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))){return;}
+
+        if(value.isPressed)
+        {
+            rb.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
     void Run()
     {
         Vector2 playerVelocity = new Vector2 (moveInput.x * speed, rb.velocity.y);
         rb.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("IsRunning", playerHasHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+
+        if(playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(rb.velocity.x), 1f);
+        }
+    }
+
+    void ClimbLadder()
+    {
+        if(!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
+        {
+            myAnimator.SetBool("IsClimbing", false);
+            rb.gravityScale = myGravity;
+            return;
+        }
+
+            bool playerHasVerticalSpeed = Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;
+            Vector2 climbVelocity = new Vector2 (rb.velocity.x, moveInput.y * climbSpeed);
+            rb.velocity = climbVelocity;
+            rb.gravityScale = climbingGravity;
+            myAnimator.SetBool("IsClimbing", playerHasVerticalSpeed);
     }
 }
